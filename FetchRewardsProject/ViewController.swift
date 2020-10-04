0151b93b99d9621f3a4ae4217f9f4366f0492484
Royce Reynolds <rossthetruth22@gmail.com>
2020-10-03 22:23:47 -0400

    Initial Commit

---
+++ FetchRewardsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchRewardsProject
+//
+//  Created by Royce Reynolds on 10/3/20.
+//  Copyright © 2020 Royce Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
